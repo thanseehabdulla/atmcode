7cfdef8194a1133f227942e218efe1eb64c8eb26
Abdulla thanseeh <thanseehabdulla@gmail.com>
2022-01-06 23:30:08 +0530

    Create atm.js

---
+++ atm.js
@@ -0,0 +1,39 @@
+function countCurrency(amount)
+    {
+    
+        let notes = [  100, 50, 20 ];
+        let noteCounter = Array(3).fill(0);
+        
+        // count notes using Greedy approach
+        for (let i = 0; i < 3; i++) {
+            if (amount >= notes[i]) {
+                noteCounter[i] = Math.floor(amount / notes[i]);
+                amount = amount - noteCounter[i] * notes[i];
+            }
+        }
+        
+        console.log('amount', amount)
+        if (amount > 0 && amount < 20) {
+        	noteCounter[1] -= 1
+          amount += 50 
+
+          if(noteCounter[2] > 0) {
+          noteCounter[2] -= 1
+          amount += 20 
+          }
+
+          if (amount >= notes[2]) {
+                console.log('amount', amount)
+                noteCounter[2] = Math.floor(amount / notes[2]);
+                amount = amount - noteCounter[2] * notes[2];
+            }
+        }
+       
+        
+        console.log(noteCounter)
+    }
+ 
+// driver code
+ 
+    let amount = 170;
+    countCurrency(amount);
